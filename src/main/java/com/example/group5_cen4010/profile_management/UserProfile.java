7bbcbc01b64ffd43553d964063b70dd01b2c8db3
Sabrina41903 <srguez022@gmail.com>
2024-09-25 18:28:39 -0400

    Sabrina created a new feature branch for profile management.

---
+++ src/main/java/com/example/group5_cen4010/profile_management/UserProfile.java
@@ -0,0 +1,7 @@
+package com.example.group5_cen4010.profile_management;
+
+public class UserProfile {
+    public static void main (String[] args) {
+        System.out.println("This is my profile management feature branch.");
+    }
+}
