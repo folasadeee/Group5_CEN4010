16b3155461b4ee51ca16c636e173d82fd8876a75
folasadeee <folasade.goddard@gmail.com>
2024-10-12 11:54:31 -0400

    Added error handing for getBooksByGenre method

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -3,8 +3,10 @@ package com.example.bookstore.controller;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,20 +17,25 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
-    @GetMapping
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    @GetMapping("/genre")
+    public List<Book> getBooksByGenre(@RequestParam String genre) {
+        genre = genre.trim();
+        System.out.println("Searching for genre: " + genre);
+        List<Book> books = bookRepository.findByGenreIgnoreCase(genre);
+
+        if (books.isEmpty() ) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No books found with genre: " + genre);
+        }
+
+        return books;
     }
 
     @PostMapping
     public Book createBook(@RequestBody Book book) {
         return bookRepository.save(book);
-    }
-
-    @GetMapping(path="/{genre}")
-    public List<Book> getBooksByGenre(@PathVariable String genre) {
-        genre = genre.trim();
-        System.out.println("Searching for genre " + genre);
-        return bookRepository.findByGenreIgnoreCase(genre);
     }
 }
