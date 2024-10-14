560103b3b7753f3090822209eaf1f67ff1f7528c
Sabrina41903 <srguez022@gmail.com>
2024-10-14 17:43:42 -0400

    Restored UserProfile and CreditCard in my branch

--- src/main/java/com/example/group5_cen4010/profile_management/UserProfile.java
+++ src/main/java/com/example/group5_cen4010/profile_management/UserProfile.java
@@ -1,33 +1,87 @@
 package com.example.group5_cen4010.profile_management;
-
 import javax.persistence.*;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 
 public class UserProfile {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "user_id")
     private Long userID;
 
-    @Column
+    @Column (name = "username")
     private String username;
 
-    @Column
+    @Column (name = "password")
     private String password;
 
-    @Column
+    @Column (name = "first_name")
     private String firstName;
 
-    @Column
+    @Column (name = "last_name")
     private String lastName;
 
-    @Column
+    @Column (name = "address")
     private String address;
 
-    //TODO: Create Getters & Setters
-    public static void main (String[] args) {
-        System.out.println("This is my profile management feature branch.");
+    @Column (name = "email")
+    private String email;
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
