d84cba319d80d8cb798e2155e8db273d4865ccaf
Sabrina41903 <srguez022@gmail.com>
2024-10-14 17:29:59 -0400

    Sabrina's CreditCard and UserProfile, resolved conflicts.

---
+++ src/main/java/com/example/group5_cen4010/profile_management/UserProfile.java
@@ -0,0 +1,87 @@
+package com.example.group5_cen4010.profile_management;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "user_id")
+    private Long userID;
+
+    @Column (name = "username")
+    private String username;
+
+    @Column (name = "password")
+    private String password;
+
+    @Column (name = "first_name")
+    private String firstName;
+
+    @Column (name = "last_name")
+    private String lastName;
+
+    @Column (name = "address")
+    private String address;
+
+    @Column (name = "email")
+    private String email;
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
