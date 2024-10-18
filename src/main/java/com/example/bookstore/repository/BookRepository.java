b39348adee08ef7de071068c7bcfcf194ba9ed0a
folasadeee <folasade.goddard@gmail.com>
2024-10-18 02:28:46 -0400

    Added support for GET top 10 sellers

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -2,11 +2,17 @@ package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, String> {
     List<Book> findByGenreIgnoreCase(String genre);
+
+
+    @Query("SELECT b FROM Book b ORDER BY b.copiesSold DESC")
+    List<Book> findTopTenSellers();
 
 }
