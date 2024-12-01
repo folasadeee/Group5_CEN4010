7326a3f5040f6f0071446b11c5740d697b52ef92
folasade <folasade.goddard@gmail.com>
2024-11-30 21:57:42 -0500

    Removed sensitive info

---
+++ src/main/java/com/example/bookstore/DotenvConfig.java
@@ -0,0 +1,18 @@
+package com.example.bookstore;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class DotenvConfig {
+
+    @PostConstruct
+    public void loadDotenv() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    }
+}
